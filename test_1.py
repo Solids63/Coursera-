e17818de717165a05e592409f4022e789eb19794
Solids63 <93522305+Solids63@users.noreply.github.com>
2022-02-18 14:39:57 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,20 +1,31 @@
 def add_dict(a, b):
-    for a, b in d.items():
-        if a in d:
-            x = d.get(a)
-#            value.append(b)
-            print(x)
-        else:
-            d[a] = [b]
-            print(d)
+ if a in d:
+        x = d.get(a)
+        x.append(b)
+    else:
+        d[a] = [b]
+        print(d)
     return d
 
 
 d = {'a': [1], 'b': [2], 'c': [3]}
 
+
 key = input("Введите ключ словаря: ")
 val = input("Введите значение ключа: ")
 
+add_dict(key, val)
+
+key = input("Введите ключ словаря: ")
+val = input("Введите значение ключа: ")
+add_dict(key, val)
+
+key = input("Введите ключ словаря: ")
+val = input("Введите значение ключа: ")
+add_dict(key, val)
+
+key = input("Введите ключ словаря: ")
+val = input("Введите значение ключа: ")
 add_dict(key, val)
 
 print(d)
